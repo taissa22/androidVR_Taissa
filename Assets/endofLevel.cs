07d9402fdb16d53f0da37d5355f17fa1512e8407
taissa22 <taissamcosta@gmail.com>
2021-01-20 19:13:26 -0300

    Cena Castelo Finalizada

---
+++ Assets/endofLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class endofLevel : MonoBehaviour
+{
+    
+    public void EndOfGame()
+    {
+        SceneManager.LoadScene("FPS");
+    }
+}
